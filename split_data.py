a456c306b51335094ad44399d29b53d3e5422e6b
ilya16 <ilyaborovik97@gmail.com>
2020-05-28 21:56:18 +0300

    Fix data_split.py

--- split_data.py
+++ split_data.py
@@ -7,12 +7,9 @@ import numpy as np
 if __name__ == '__main__':
     parser = argparse.ArgumentParser('Meme dataset split')
 
-    parser.add_argument('--source', '-s', type=str, default='memegenerator.net',
-                        help='data source')
     parser.add_argument('--data-dir', '-d', required=True, type=str,
                         help='directory with the dataset')
-
-    parser.add_argument('--splits', type=int, default=(2500, 250, 250), nargs='3',
+    parser.add_argument('--splits', type=int, default=(2500, 250, 250), nargs=3,
                         help='sizes of train/val/test splits for each template')
     parser.add_argument('--random-state', type=int, default=0,
                         help='random seed for the data shuffling')
@@ -21,6 +18,8 @@ if __name__ == '__main__':
     assert args.source == 'memegenerator.net', 'Only memegenerator.net is supported'
 
     np.random.seed(0)
+    start_ids = np.cumsum([0] + args.splits)
+    end_ids = start_ids[1:]
 
     labels, captions = defaultdict(bool), defaultdict(list)
     with open(os.path.join(args.data_dir, 'captions.txt'), 'r') as f:
@@ -29,23 +28,19 @@ if __name__ == '__main__':
             captions[label].append(line)
             labels[label] = True
 
-    f_train = open(os.path.join(args.data_dir, 'captions_train.txt'), 'w')
-    f_val = open(os.path.join(args.data_dir, 'captions_val.txt'), 'w')
-    f_test = open(os.path.join(args.data_dir, 'captions_test.txt'), 'w')
+    splits = ['train', 'val', 'test']
+    f_splits = [
+        open(os.path.join(args.data_dir, f'captions_{split}.txt'), 'w')
+        for split in splits
+    ]
 
     for label in labels.keys():
         indices = np.arange(len(captions[label]))
         np.random.shuffle(indices)
 
-        for idx in sorted(indices[:args.splits[0]]):
-            f_train.write(captions[label][idx])
+        for i, f in enumerate(f_splits):
+            for idx in sorted(indices[start_ids[i]:end_ids[i]]):
+                f.write(captions[label][idx])
 
-        for idx in sorted(indices[args.splits[0]:args.splits[1]]):
-            f_val.write(captions[label][idx])
-
-        for idx in sorted(indices[args.splits[1]:]):
-            f_test.write(captions[label][idx])
-
-    f_train.close()
-    f_val.close()
-    f_test.close()
+    for f in f_splits:
+        f.close()
